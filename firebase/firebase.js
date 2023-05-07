5766b8b84505b93a33ac9e2ee79a03b5bc6d1111
unknown <bgw595@myy.haaga-helia.fi>
2023-05-07 19:15:06 +0300

    Readme file was added and firebase configs are hidden now

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCbC1bTIbpFsZJRtPWvetywpsnO5hIfwS0',
-    authDomain: 'mytest-585af.firebaseapp.com',
-    projectId: 'mytest-585af',
-    storageBucket: 'mytest-585af.appspot.com',
-    messagingSenderId: '633605322194',
-    appId: '1:633605322194:web:63344a64ab9b67db1c440b'
+    //configs are hidden :)
 };
 
 // Initialize Firebase
